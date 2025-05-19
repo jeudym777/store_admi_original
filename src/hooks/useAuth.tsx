8d133a65b808e06c3b168becf8ee714ed2876d2b
Aluzed <alexandre.penombre@gmail.com>
2025-05-19 09:03:25 +0200

    explicit destructuring on context

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,4 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "@/context/AuthContext";
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+  const { user, isLoading } = useContext(AuthContext);
+  return { user, isLoading };
+};
